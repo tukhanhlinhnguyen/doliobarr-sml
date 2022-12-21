636450fcd945daddd099678a1290bbc0d7e15ee5
NGUYEN TU KHANH LINH <ntklsideproject@gmail.com>
2022-12-21 10:40:00 +0100

    Init

---
+++ conf/conf.php
@@ -0,0 +1,24 @@
+<?php
+$dolibarr_main_instance_unique_id='23c975a58b8741918142baa20a659cf4';
+$dolibarr_main_url_root='http://gestionsml.societe-kali.fr';
+$dolibarr_main_document_root='/home/u877895869/domains/societe-kali.fr/public_html/gestionsml';
+$dolibarr_main_data_root='/home/u877895869/domains/societe-kali.fr/public_html/gestionsml/documents';
+$dolibarr_main_db_host='127.0.0.1';
+$dolibarr_main_db_port="";
+$dolibarr_main_db_name='u877895869_ezjAh';
+$dolibarr_main_db_prefix='r2aw_';
+$dolibarr_main_db_user='u877895869_3Yd7i';
+$dolibarr_main_db_pass='rdT9HGA4D5';
+$dolibarr_main_db_type='mysqli';
+$dolibarr_main_db_character_set="utf8";
+$dolibarr_main_db_collation="utf8_unicode_ci";
+$dolibarr_main_authentication="dolibarr";
+
+// Specific settings
+$dolibarr_main_force_https="0";
+$dolibarr_main_prod="0";
+$dolibarr_main_restrict_os_commands='mysqldump, mysql, pg_dump, pgrestore';
+$dolibarr_main_restrict_ip='';
+$dolibarr_nocsrfcheck="0";
+$dolibarr_mailing_limit_sendbyweb="0";
+?>
